2084f61c7ccd3ebfc6b57f6171d7a966092be501
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-10-29 01:31:29 -0300

    Editando o docker_build_and_run.sh

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
-docker build -t angular-docker .
+#############################################
+### Shellscript to up Docker's containers ###
+#############################################
 
-docker run -p 4201:4200 angular-docker
+echo -e "\033[01;32m##############\033[01;32m"
+echo -e "\033[01;32m### Docker ###\033[01;32m"
+echo -e "\033[01;32m##############\033[01;32m"
+echo -e "\n"
+
+docker_image=$(docker images angular)
+
+if [[ ! -z "${docker_image}" ]]; then
+	echo "\033[01;32mApagando imagem que roda a aplicação....\033[01;32m"
+	echo -e "\n"
+	docker rmi -f $docker_image
+	echo -e "\n"
+fi
+
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\033[01;32m### Building containers ###\033[01;32m"
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\n\n"
+
+sudo docker-compose build
+
+echo -e "\n\n"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\033[01;32m### Uping containers ###\033[01;32m"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\n\n"
+
+sudo docker-compose up -d
